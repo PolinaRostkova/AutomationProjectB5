3288e9b3a7ae32ef7e6cc7445af667e9541602e0
PolinaRostkova <polina.rostkova02@gmail.com>
2025-08-10 15:20:18 -0400

    xpath, findElements, wait

--- src/test/java/io/loop/test/utilities/GeneralConstants.java
+++ src/test/java/io/loop/test/utilities/GeneralConstants.java
@@ -6,4 +6,7 @@ public class GeneralConstants {
     public static final String CHROME = "Chrome";
     public static final String FIREFOX = "Firefox";
     public static final String SAFARI = "Safari";
+
+    // herokuapp url
+    public static final String HEROKUAPP_URL = "https://the-internet.herokuapp.com/forgot_password";
 }
