b9528cf3c645879f31f3b586f8c03b93ccd98001
PolinaRostkova <polina.rostkova02@gmail.com>
2025-08-21 21:51:48 -0400

    day 4

---
+++ src/test/java/io/loop/test/utilities/ConfigurationReader.java
@@ -0,0 +1,29 @@
+package io.loop.test.utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+
+    private static final Properties properties;
+
+    static {
+        try{
+            String path = "configuration.properties";
+            FileInputStream input = new FileInputStream(path);
+            properties = new Properties();
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+
+}
